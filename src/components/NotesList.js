a69caa3a484ee59f34a9e71d1cb68153331f315f
arjun rehal <113398985+reha3125@users.noreply.github.com>
2022-10-30 18:59:52 -0400

    Add files via upload
    assignment 3 client side note

---
+++ src/components/NotesList.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Note from './Note'
+import AddNote from './AddNote'
+
+const NotesList = ({
+	notes,
+	handleAddNote,
+	handleDeleteNote,
+	handleUpdateNote,
+}) => {
+	return (
+		<div className='notes-list'>
+			{notes.map((note) => (
+				<Note
+					key={note.id}
+					id={note.id}
+					text={note.text}
+					dateCreated={note.dateCreated}
+					lastModified={note.lastModified}
+					handleDeleteNote={handleDeleteNote}
+					handleUpdateNote={handleUpdateNote}
+				/>
+			))}
+			<AddNote handleAddNote={handleAddNote}/>
+		</div>
+	)
+}
+
+export default NotesList
